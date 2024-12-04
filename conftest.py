8003ba849f58cd1e2f364f4f344ba19dda710728
mashal7 <mashal7@mail.ru>
2024-12-04 16:00:08 +0300

    new

--- conftest.py
+++ conftest.py
@@ -3,66 +3,62 @@ import pytest
 from datetime import datetime
 pytest_plugins = ["pytest_playwright"]
 
+@pytest.fixture()
+def browser_fixture():
+    return {
+        "viewport": {
+            "width": 1920,
+            "height": 1080,
+        }
+    }
 
-# Хук для создания скриншотов в случае, если тест завершается с ошибкой
-@pytest.hookimpl(tryfirst=True, hookwrapper=True)
-def pytest_runtest_makereport(item, call):
-    """
-    Хук pytest для создания скриншота в случае провала теста.
-    Выполняется после каждого теста, анализируя его результат.
-    """
-    outcome = yield  # Приостанавливает выполнение до завершения теста
-    report = outcome.get_result()  # Получает отчет о выполнении теста
-
-    # Проверяем, завершился ли тест с ошибкой
-    if report.when == "call" and report.failed:
-        # Получаем объект 'page' из аргументов теста, если он был передан через фикстуру
-        page = item.funcargs.get("page", None)
-
-        if page:  # Если страница была создана и передана в тест
-            # Генерируем путь для сохранения скриншота
-            timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-            folder_path = os.path.abspath('screens')
-            screenshot_path = os.path.join(folder_path, f"{item.name}_{timestamp}.png")
-
-            # Делаем скриншот текущего состояния страницы
-            page.screenshot(path=screenshot_path)
-
-            # Выводим сообщение о сохранении скриншота в консоль
-            print(f"Screenshot saved for failed test: {screenshot_path}")
-
-
-# @pytest.fixture()
-# def browser_fixture():
-#     return {
-#         "viewport": {
-#             "width": 1920,
-#             "height": 1080,
-#         }
-#     }
-
-# настройка фикстуры для управления браузером вручную
-
-import pytest
-from playwright.sync_api import sync_playwright
-
-# Фикстура для браузера, открывается один раз на всю сессию
+# import pytest
+# from playwright.sync_api import sync_playwright
+#
+# # Фикстура для браузера, открывается один раз на всю сессию
 # @pytest.fixture(scope="session")
 # def browser():
 #     playwright = sync_playwright().start()
 #     browser = playwright.chromium.launch(headless=False)  # Открываем браузер
-#     browser.new_context(screenshot_on_failure=True)
 #     yield browser
-#     # Не закрываем браузер в этой фикстуре, если хотите, чтобы он остался открытым
-#     # browser.close()  # Уберите эту строку, чтобы не закрывать браузер
+#     browser.close()  # Уберите эту строку, чтобы не закрывать браузер
 #     playwright.stop()
 #
-# # Фикстура для страницы, открывается для каждого теста
+# # # Фикстура для страницы, открывается для каждого теста
 # @pytest.fixture(scope="function")
 # def page(browser):
 #     context = browser.new_context()  # Создаем новый контекст
 #     page = context.new_page()  # Создаем новую страницу
 #     yield page
-#     # Не закрываем контекст после теста, чтобы оставить его открытым
-#     # context.close()  # Уберите эту строку, чтобы контекст не закрывался
+#     context.close()  # Уберите эту строку, чтобы контекст не закрывался
+
+# Хук для создания скриншотов в случае, если тест завершается с ошибкой
+# @pytest.hookimpl(tryfirst=True, hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     """
+#     Хук pytest для создания скриншота в случае провала теста.
+#     Выполняется после каждого теста, анализируя его результат.
+#     """
+#     outcome = yield  # Приостанавливает выполнение до завершения теста
+#     report = outcome.get_result()  # Получает отчет о выполнении теста
+#
+#     # Проверяем, завершился ли тест с ошибкой
+#     if report.when == "call" and report.failed:
+#         # Получаем объект 'page' из аргументов теста, если он был передан через фикстуру
+#         page = item.funcargs.get("page", None)
+#
+#         if page:  # Если страница была создана и передана в тест
+#             # Генерируем путь для сохранения скриншота
+#             timestamp = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+#             folder_path = os.path.abspath('screens')
+#             screenshot_path = os.path.join(folder_path, f"{item.name}_{timestamp}.png")
+#
+#             # Делаем скриншот текущего состояния страницы
+#             page.screenshot(path=screenshot_path)
+#
+#             # Выводим сообщение о сохранении скриншота в консоль
+#             print(f"Screenshot saved for failed test: {screenshot_path}")
+
+
+
 
--- test_assert.py
+++ test_assert.py
@@ -5,6 +5,11 @@ from pyexpat.errors import messages
 
 # Фикстура page предоставляет новую веб-страницу для запуска теста и функции для работы с ней
 def test_locators(page) -> None:
+
+    # перехватывать сетевой трафик
+    page.on("request", lambda request: print(">>", request.method, request.url))
+    page.on("response", lambda response: print("<<", response.status, response.url))
+
     page.goto("https://demo.playwright.dev/todomvc/#/")
 
     # проверка url
@@ -19,12 +24,14 @@ def test_locators(page) -> None:
     field_input.press('Enter')
     field_input.fill('number 2')
     field_input.press('Enter')
+
+    #page.pause()
+
     items_locator = page.get_by_test_id('todo-item')
     expect(items_locator).to_have_count(2)
 
     # отметить одну задачу выполненной и проверить что она выполнена
     items_locator.get_by_role('checkbox').nth(0).click()
-    expect(items_locator.nth(0)).to_have_class('complete')
+    expect(items_locator.nth(0)).to_have_class('completed')
 
-
-    input("Нажмите Enter для продолжения...")
+    #input("Нажмите Enter для продолжения...")
